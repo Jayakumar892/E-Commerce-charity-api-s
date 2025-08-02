ff0238af52594f9db5509849ccafcced1da2eb84
jaya <jayajaya6858@gmail.com>
2025-08-02 13:26:05 +0530

    category

--- routes/category.js
+++ routes/category.js
@@ -3,9 +3,15 @@ const router = express.Router();
 const { authenticateUser } = require("../middlewares/auth");
 const { uploadImage } = require("../middlewares/multer");
 const { categoryValidationSchema, validateCategory } = require("../middlewares/validationCategory");
-const { createCategory } = require("../controllers/createCategory");
+const { createCategory,
+    getAllCategories,
+    getAdminCategories,
+    updateCategory } = require("../controllers/createCategory");
 
 
 router.post( "/create", authenticateUser, uploadImage, categoryValidationSchema, validateCategory, createCategory);
+router.get("/", getAllCategories);
+router.get("/admin", authenticateUser, getAdminCategories);
+router.patch("/", authenticateUser, updateCategory);
 
 module.exports = router;
